0d0f8c919bd20392864df9fe1b6b0b12cf5becb1
RicksLWilliams <rlwilliamsix@yahoo.com>
2020-07-09 12:27:57 -0500

    gold, steel, titanium

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let cheese = 0;
 let clickBonus = 1;
 let autoBonus = 0;
+let clickCount = 0;
+
 
 let clickUpgrades = {
   pickaxes: {
@@ -26,7 +28,28 @@ let clickUpgrades = {
     quantity: 0,
     multiplier: 4,
     type: "auto"
-  }
+  },
+  gold: {
+    price: 0,
+    quantity: 1,
+    multiplier: 1,
+    amount:0,
+    type: "click"
+  },
+  steel: {
+    price: 0,
+    quantity: 1,
+    multiplier: 1,
+    amount:0,
+    type: "click"
+  },
+  titanium: {
+    price: 0,
+    quantity: 1,
+    multiplier: 1,
+    amount:0,
+    type: "click"
+  },
 };
 
 
@@ -40,23 +63,24 @@ function mine() {
 function clickUpgrade(item) {
   let elm = clickUpgrades[item];
 
-  if (cheese < elm.price) { 
+  clickCount++
+
+
+  if (elm.amount < elm.price) { 
     return 
   }
-    cheese -= elm.price 
-    elm.quantity++
-    elm.price *= 2
+    elm.amount += elm.quantity 
 
-    document.getElementById(item).innerText =  item + " for " + (elm.price).toString()
-    document.getElementById(item + "-count").innerText = (elm.quantity).toString()
-    document.getElementById(item + "-bonus").innerText = "(+" + (elm.quantity * elm.multiplier).toString() + ")"
-    document.getElementById("cheese").innerText = cheese.toString()
+    //document.getElementById(item).innerText =  item + " for " + (elm.price).toString()
+    document.getElementById(item + "-quantity").innerText = (elm.quantity).toString()
+    document.getElementById(item + "-amount").innerText = "(+" + (elm.amount).toString() + ")"
+    document.getElementById("cheese").innerText = clickCount.toString()
 
-    if (elm.type == "click") {
-      clickBonus += elm.multiplier
-    } else {
-      autoBonus +=elm.multiplier
-    }
+    //if (elm.type == "click") {
+    //  clickBonus += elm.multiplier
+    //} else {
+    //  autoBonus +=elm.multiplier
+    //}
   
 }
 
